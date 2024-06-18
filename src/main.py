9f26ac79114b4eff0c7d2ba2c3854bd48feb3e95
Colin Dugeai <colin.dugeai@gmail.com>
2024-06-18 12:38:42 +0200

    Update main.py

--- src/main.py
+++ src/main.py
@@ -2,16 +2,16 @@ from dotenv import load_dotenv
 from utils.api_pilote import Pilote
 from os import environ
 
-
+load_dotenv()
 
 pylote_dev = Pilote(environ.get("BASE_URL"), environ.get("API_KEY"))
 
 
-res1 = pylote_dev.get_donnees_indicateur("CH-047", "IND-208")
+donnees_208 = pylote_dev.get_donnees_indicateur("CH-047", "IND-208")
 
 print({
-    "indic_id": res1['indic_id'],
-    "est_barometre": res1['est_barometre'],
-    "chantier_id": res1['chantier_id'],
-    "donnees_territoires": len(res1['donnees_territoires']),
+    "indic_id": donnees_208['indic_id'],
+    "est_barometre": donnees_208['est_barometre'],
+    "chantier_id": donnees_208['chantier_id'],
+    "donnees_territoires": len(donnees_208['donnees_territoires']),
 })
