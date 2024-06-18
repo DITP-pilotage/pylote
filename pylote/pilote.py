ffa8c6437d4875c02964f26cee3ddae7308bc9b3
Vincent Lara <lara.vincent@gmail.com>
2024-06-18 17:33:30 +0200

    Python package

--- pylote/pilote.py
+++ pylote/pilote.py
@@ -1,3 +1,5 @@
+from urllib.parse import urljoin
+
 import requests
 
 class Pilote:
@@ -7,12 +9,17 @@ class Pilote:
         self.api_key = api_key
         return
 
+    @property
+    def headers(self):
+        return {"Authorization": f"Bearer {self.api_key}"}
+
     def get_donnees_indicateur(self, chantier_id, indicateur_id):
         endpoint_url="/".join(["chantier", chantier_id, "indicateur", indicateur_id, "donnees"])
-        
+        full_url = urljoin(self.base_url, endpoint_url)
+
         res = requests.get(
-            "/".join([self.base_url, endpoint_url]),
-            headers= {"Authorization":"Bearer "+self.api_key}
+            full_url,
+            headers=self.headers
         ).json()
 
         return res
